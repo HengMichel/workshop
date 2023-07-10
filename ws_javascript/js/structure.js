158b6d9f08537aaedbd27374b6f0524dbcaeb8e6
cavacmoi <cavacmoi@gmail.com>
2023-07-10 21:29:13 +0200

    workshopUpdate

--- ws_javascript/js/structure.js
+++ ws_javascript/js/structure.js
@@ -1,6 +1,6 @@
 const myCarouselElement = document.querySelector("#michelCarousel")
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 5000,
+  interval: 3000,
   touch: false
 })
